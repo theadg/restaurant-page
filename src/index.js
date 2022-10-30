04ccd1edeebfd22d054128240fcb62a4df6d64c3
theadg <deguzmanbernardandrew@gmail.com>
2022-10-30 09:43:27 +0800

    Set hero as first tab

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import "./styles/main.scss";
 // import adobo from "./assets/adobo.jpg";
 import Navbar from "./Components/navbar";
 import Hero from "./Components/hero";
-import Menu from "./Components/menu";
-import Contact from "./Components/contact";
+
 const container = document.querySelector("#content");
 export const tabContainer = document.createElement("div");
 tabContainer.id = "tabContainer";
 container.appendChild(Navbar());
-tabContainer.appendChild(Contact());
+tabContainer.appendChild(Hero());
 container.appendChild(tabContainer);
 
 export function removeChildren() {
